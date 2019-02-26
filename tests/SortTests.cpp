0bfde44c13cd9c59414457edeb88a000439d6b43
samuelh <samuelh@graphcore.ai>
2019-02-26 21:25:37 +0000

    add and sort tests

--- tests/SortTests.cpp
+++ tests/SortTests.cpp
@@ -44,6 +44,47 @@ static void random_sort_test(const unsigned seed, const std::size_t n, const int
   }
 }
 
+static void random_multiply_and_addition(const unsigned seed) {
+  std::mt19937 random_engine(seed);
+  const unsigned num_add = (random_engine() % 20) + 2;
+
+  std::vector<std::vector<int>> reference(num_add);
+  for (unsigned i = 0; i < reference.size(); ++i) {
+    const unsigned num_mult = (random_engine() % 20) + 2;
+    for (unsigned j = 0; j < num_mult; ++j) {
+      reference[i].push_back((random_engine() % 500));
+    }
+  }
+  
+  Expression exp;
+  for (const auto & vec : reference) {
+    Expression mul_term =  Operator(std::to_string(vec[0]), ordering_value(vec[0]), operator_id(vec[0]))
+                           * Operator(std::to_string(vec[1]), ordering_value(vec[1]), operator_id(vec[1]));
+    for (unsigned j = 2; j < vec.size(); ++j) {
+      mul_term = mul_term * Operator(std::to_string(vec[j]), ordering_value(vec[j]), operator_id(vec[j]));
+    }
+    exp = exp + mul_term;
+  } 
+
+  for (unsigned i = 0; i < reference.size(); ++i) {
+    for (unsigned j = 0; j < reference[i].size(); ++j) {
+      ASSERT_EQ(exp.expression[i][j].order.value, reference[i][j]);
+    }
+  }
+
+  for (auto & vec : reference) {
+    std::sort(vec.begin(), vec.end());
+  }
+  const auto new_exp = exp.sort(commute_all);
+
+  for (unsigned i = 0; i < reference.size(); ++i) {
+    for (unsigned j = 0; j < reference[i].size(); ++j) {
+      EXPECT_EQ(new_exp.expression[i][j].order.value, reference[i][j]);
+      EXPECT_EQ(new_exp.expression[i][j].order.value, new_exp.expression[i][j].id.value);
+    }
+  }
+}
+
 TEST(sort_tests, empty) {
   Expression exp;
   auto new_exp = exp.sort(commute_all);
@@ -75,6 +116,17 @@ TEST_P (RandomSortTest, random_sort) {
 
 INSTANTIATE_TEST_SUITE_P(RandomSortingTests, RandomSortTest, testing::Range(3U, 100U));
 
+class AdditionAndMultiplyRandomSort : public ::testing::TestWithParam<unsigned> {
+  // You can implement all the usual class fixture members here.
+};
+
+TEST_P(AdditionAndMultiplyRandomSort, random_add_sort) {
+  const unsigned m = GetParam();
+  const unsigned seed = m + 1000; // don't use same seed as random sort tests 
+  random_multiply_and_addition(seed);
+}
+
+INSTANTIATE_TEST_SUITE_P(AdditionAndMultiplyTests, AdditionAndMultiplyRandomSort, testing::Range(0U, 30U));
 
 int main(int argc, char **argv) {
   ::testing::InitGoogleTest(&argc, argv);
