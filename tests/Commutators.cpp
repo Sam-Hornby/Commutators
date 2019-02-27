4c061b1cd86d55f203182d34c540349bc1e56a3f
samuelh <samuelh@graphcore.ai>
2019-02-27 09:02:11 +0000

    four test

--- tests/Commutators.cpp
+++ tests/Commutators.cpp
@@ -65,6 +65,22 @@ TEST(commutator_tests, commute_three_number) {
   ASSERT_EQ(ss.str(), "(2 * B * C * A) + (2 * C * [A, B]) + (2 * B * [A, C]) + (2 * [[A, B], C])\n");
 }
 
+TEST(commutator_tests, commute_four) {
+  Operator A("A", ordering_value(-1), operator_id(-1));
+  Operator B("B", ordering_value(-1), operator_id(-1));
+  Operator C("C", ordering_value(-1), operator_id(-1));
+  Operator D("D", ordering_value(-2), operator_id(-2));
+
+  auto exp = A * B * C * D;
+  exp = exp.sort(commute_none);
+
+  std::stringstream ss;
+  exp.print(ss);
+  ASSERT_EQ(ss.str(), "(D * A * B * C) + (A * B * [C, D]) + (A * C * [B, D]) + "
+                      "(B * C * [A, D]) + (A * [[B, D], C]) + (C * [[A, D], B]) + "
+                      "(B * [[A, D], C]) + ([[[A, D], B], C])\n");
+}
+
 int main(int argc, char **argv) {
   ::testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
