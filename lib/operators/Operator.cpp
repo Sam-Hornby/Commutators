a2c067e4a906b7b55295eeeabf52aa6c5319ab1c
samuelh <samuelh@graphcore.ai>
2019-09-15 05:34:20 +0100

    delete the old

--- lib/operators/Operator.cpp
+++ lib/operators/Operator.cpp
@@ -1,28 +1,2 @@
 
 #include "Operator.hpp"
-
-/*namespace operators {
-
-Expression operator+(const Operator & A, const Operator & B) {
-  Expression exp;
-  exp.expression.resize(2);
-  exp.expression[0].push_back(A);
-  exp.expression[1].push_back(B);
-  return exp;
-}
-
-Expression operator*(const Operator & A, const Operator & B) {
-  Expression exp;
-  std::vector<Operator> m;
-  m.push_back(A);
-  m.push_back(B);
-  exp.expression.push_back(std::move(m));
-  return exp;
-}
-
-Operator number(const double n) {
-  return Operator(n);
-}
-
-
-}*/ // end namespace
