7a7c9dedb3575fc95878ade0cf3b6be203b9bef8
samuelh <samuelh@graphcore.ai>
2019-09-29 00:43:44 +0100

    forgotten files

---
+++ include/operators/ImaginaryNumber.hpp
@@ -0,0 +1,66 @@
+#ifndef _IM_NUMBER_
+#define _IM_NUMBER_
+
+namespace operators {
+
+struct ImaginaryNumber {
+  double value;
+  explicit ImaginaryNumber(double val) : value(val) {}
+};
+
+struct ComplexNumber {
+  double real_part = 0;
+  double imaginary_part = 0;
+  ComplexNumber() = default;
+  ComplexNumber(double a) : real_part(a) {}
+  ComplexNumber(ImaginaryNumber a) : imaginary_part(a.value) {}
+  ComplexNumber(double a, ImaginaryNumber b) : real_part(a), imaginary_part(b.value) {}
+  ComplexNumber(double r, double im) : real_part(r), imaginary_part(im) {}
+  std::string name() const {
+    if (imaginary_part == 0) {
+      return std::to_string(real_part);
+    }
+    if (real_part == 0) {
+      return std::to_string(imaginary_part) + " * i";
+    }
+    return "(" + std::to_string(real_part) + " + " + std::to_string(imaginary_part) + "i)";
+  }
+  bool operator==(const ComplexNumber &other) {
+    return std::tie(real_part, imaginary_part) == std::tie(other.real_part, other.imaginary_part);
+  }
+  bool operator!=(const ComplexNumber &other) {
+    return not (*this == other);
+  }
+};
+
+template <typename T>
+ImaginaryNumber operator*(ImaginaryNumber im, T r) {
+  static_assert(not std::is_same<T, ImaginaryNumber>::value, "Should use overload version");
+  return ImaginaryNumber(im.value * r);
+}
+double operator*(ImaginaryNumber A, ImaginaryNumber B) {
+  return A.value * B.value * -1;
+}
+ComplexNumber operator*(ComplexNumber A, ComplexNumber B) {
+  return ComplexNumber((A.real_part * B.real_part) - (A.imaginary_part * B.imaginary_part),
+                        (A.real_part * B.imaginary_part) + (A.imaginary_part * B.real_part));
+}
+ComplexNumber operator*(ImaginaryNumber A, ComplexNumber B) {
+  return ComplexNumber(A) * B;
+}
+ComplexNumber operator*(double A, ComplexNumber B) {
+  return ComplexNumber(A) * B;
+}
+
+ComplexNumber operator+(ComplexNumber A, ComplexNumber B) {
+  return ComplexNumber(A.real_part + B.real_part, A.imaginary_part + B.imaginary_part);
+}
+ComplexNumber operator+(ImaginaryNumber A, ComplexNumber B) {
+  return ComplexNumber(A) + B;
+}
+ComplexNumber operator+(double A, ComplexNumber B) {
+  return ComplexNumber(A) + B;
+}
+
+}
+#endif
---
+++ tests/Examples/SimpleFermions.cpp
@@ -0,0 +1,54 @@
+#include "gtest/gtest.h"
+#include "Expression.hpp"
+#include "Substitutions.hpp"
+#include "Ordering.hpp"
+#include "Utils.hpp"
+#include "Operator.hpp"
+#include "Comutators.hpp"
+#include <exception>
+#include <cmath>
+
+using namespace operators;
+
+static double round_to_3_dp(double x) {
+  return round(x * 1000) / 1000;
+}
+
+template <class OperatorInfo>
+static bool check_answer(const Expression<OperatorInfo> & exp, const ComplexNumber ans) {
+  if (exp.expression.size() != 1) {
+    return false;
+  }
+  if (exp.expression[0].size() != 1) {
+    return false;
+  }
+  if (!exp.expression[0][0].is_number()) {
+    return false;
+  }
+  return round_to_3_dp(exp.expression[0][0].value().real_part) == round_to_3_dp(ans.real_part) and
+         round_to_3_dp(exp.expression[0][0].value().imaginary_part) == round_to_3_dp(ans.imaginary_part);
+}
+
+TEST(Fermions, simple) {
+  Expression<Fock1DInfo> exp1 = creation_op(0) * anihilation_op(0) * creation_op(0);
+  Expression<Fock1DInfo> exp2 = creation_op(0) * anihilation_op(0) * anihilation_op(0);
+  Expression<Fock1DInfo> exp3 = creation_op(0) * creation_op(0) * anihilation_op(0);
+  Expression<Fock1DInfo> exp4 = anihilation_op(0) * creation_op(0);
+  exp1 = normal_order<Fock1DInfo>(exp1);
+  exp2 = normal_order<Fock1DInfo>(exp2);
+  exp3 = normal_order<Fock1DInfo>(exp3);
+  exp4 = normal_order<Fock1DInfo>(exp4);
+  exp1 = exp1.evaluate(boson_commutator<Fock1DInfo>, fermion_dual_occupation<Fock1DInfo>, SortUsing::ANTICOMMUTATORS);
+  exp2 = exp2.evaluate(boson_commutator<Fock1DInfo>, fermion_dual_occupation<Fock1DInfo>, SortUsing::ANTICOMMUTATORS);
+  exp3 = exp3.evaluate(boson_commutator<Fock1DInfo>, fermion_dual_occupation<Fock1DInfo>, SortUsing::ANTICOMMUTATORS);
+  exp4 = exp4.evaluate(boson_commutator<Fock1DInfo>, fermion_dual_occupation<Fock1DInfo>, SortUsing::ANTICOMMUTATORS);
+  exp1.print();
+  exp2.print();
+  exp3.print();
+  exp4.print();
+}
+
+int main(int argc, char **argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
