26b552dd6befccfbaa87e7b09ce50fd8e142735f
samuelh <samuelh@graphcore.ai>
2019-10-07 02:11:11 +0100

    fock1dinfo overloads

--- include/transforms/BogoliubovTransform.hpp
+++ include/transforms/BogoliubovTransform.hpp
@@ -17,14 +17,38 @@ enum class OperatorType {
 
 template <class InfoA>
 bool isSecondOp(const Operator<InfoA> & op) [
+  // Will give a default but suspect for most custom classes, an overload
+  // for that template instance will be needed.  Same for the create function
   std::abort();
-  // TODO work out be way for this
-]
+}
+
+template <>
+bool isSecondOp(const Operator<Fock1DInfo> & op) {
+  assert(op.info().x_coordinate < 2);
+  return op.info().x_coordinate == 1;
+}
 
 template <class InfoA>
-Operator<InfoA> createBogOp(const Operator<InfoA> & op, const bool swap) {
+Operator<InfoA> createBogOp(Operator<InfoA> op, const bool swap) {
   std::abort();
   // TODO work out nice way for this
+}
+
+template <>
+Operator<Fock1DInfo> createBogOp(Operator<Fock1DInfo> op, const bool swap) {
+  assert(op.info().x_coordinate < 2);
+  assert(isFockOpType(op.info().type));
+  if (op.info().symbol != 'd') {
+    op.info().symbol = 'd';
+  } else {
+    op.info().symbol = 'c';
+  }
+  if (swap) {
+    op.info().x_coordinate = (op.info().x_coordinate xor 1);
+    op.info().type = op.info().type == Type::CREATION_OPERATOR ?
+                                         Type::ANIHILATION_OPERATOR : Type::CREATION_OPERATOR;
+  }
+  return op;
 }
 
 template <class InfoA>
@@ -35,7 +59,7 @@ bogoliubov_transform(const Expression<InfoA> & input,
                      const OperatorType type) {
   return transform_expression<InfoA, InfoA>([&] (const Operator<InfoA> & op) {
     if (op.is_number() or isVectorType(op.info().type) {
-      return {{{op)}}};
+      return {{{op}}};
     }
     if (not isFockOpType(op.info().type)) {
       throw std::logic_error("Unexpected type");
