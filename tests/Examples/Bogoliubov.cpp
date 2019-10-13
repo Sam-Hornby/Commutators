795a741f6ea9f1363bdcee8e10241f7d878ef385
Sam Hornby <samuelh@graphcore.ai>
2019-10-13 22:22:30 +0100

    bogoliubov test file

---
+++ tests/Examples/Bogoliubov.cpp
@@ -0,0 +1,9 @@
+#include "transforms/BogoliubovTransform.hpp"
+#include "struct_ids.hpp"
+#include "Comutators.hpp"
+#define BOOST_TEST_MODULE Bogoliubov
+#include <boost/test/included/unit_test.hpp>
+
+BOOST_AUTO_TEST_CASE(Simple_2_case) {}
+
+BOOST_AUTO_TEST_CASE(Weakly_interacting_bose_gas) {}
