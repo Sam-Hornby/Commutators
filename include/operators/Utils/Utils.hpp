80f6a972ed35c861a157a25dc68189da5029ff4d
Sam Hornby <samuelh@graphcore.ai>
2021-10-28 17:51:11 +0100

    util

--- include/operators/Utils/Utils.hpp
+++ include/operators/Utils/Utils.hpp
@@ -1,12 +1,12 @@
 #pragma once
 
-#include <Expression/Operator.hpp>
-#include <Expression/struct_ids.hpp>
 #include <Expression/Expression.hpp>
 #include <Expression/Infos/Fock1D.hpp>
+#include <Expression/Operator.hpp>
+#include <Expression/struct_ids.hpp>
+#include <Numbers/CompositeNumberExpressions.hpp>
 #include <cmath>
 #include <math.h>
-#include <Numbers/CompositeNumberExpressions.hpp>
 
 namespace operators {
 
@@ -17,20 +17,23 @@ inline Operator<OperatorInfo> vacuum_state() {
 
 inline Operator<Fock1DInfo> creation_op(const int info_value,
                                         const char sym = 'a') {
-  return Operator<Fock1DInfo>(ordering_value(0),
-                  Fock1DInfo(info_value, 0, Type::CREATION_OPERATOR, sym));
+  return Operator<Fock1DInfo>(
+      ordering_value(0),
+      Fock1DInfo(info_value, 0, Type::CREATION_OPERATOR, sym));
 }
 
 inline Operator<Fock1DInfo> anihilation_op(const int info_value) {
-  return Operator<Fock1DInfo>(ordering_value(0),
-                  Fock1DInfo(info_value, Type::ANIHILATION_OPERATOR));
+  return Operator<Fock1DInfo>(
+      ordering_value(0), Fock1DInfo(info_value, Type::ANIHILATION_OPERATOR));
 }
 
 template <class Info>
-Expression<Info> normalised_n_occupied_state(unsigned n, int creation_op_index) {
+Expression<Info> normalised_n_occupied_state(unsigned n,
+                                             int creation_op_index) {
   Expression<Info> exp;
   exp.expression.push_back({vacuum_state<Info>()});
-  double normalisation_factor = 1.0 / std::sqrt(tgamma(static_cast<double>(n + 1)));
+  double normalisation_factor =
+      1.0 / std::sqrt(tgamma(static_cast<double>(n + 1)));
   for (unsigned i = 0; i < n; ++i) {
     exp = creation_op(creation_op_index) * exp;
   }
@@ -42,9 +45,9 @@ Expression<Info> normalised_n_occupied_ops(unsigned n, int creation_op_index,
                                            const char sym = 'a') {
   Expression<Info> exp;
   exp.expression.push_back({number<Info>(1.0)});
-  double normalisation_factor = 1.0 / std::sqrt(tgamma(static_cast<double>(n + 1)));
-  for (unsigned i = 0; i < n; ++i) {
-    exp = creation_op(creation_op_index, sym) * exp;
+  double normalisation_factor = 1.0 / std::sqrt(tgamma(static_cast<double>(n +
+1))); for (unsigned i = 0; i < n; ++i) { exp = creation_op(creation_op_index,
+sym) * exp;
   }
   return number<Info>(normalisation_factor) * exp;
 }*/
@@ -55,7 +58,8 @@ Expression<Info> normalised_n_occupied_ops(unsigned n, Args... args) {
   Operator<Info> op(ordering_value(0), info);
   Expression<Info> exp;
   exp.expression.push_back({number<Info>(1.0)});
-  double normalisation_factor = 1.0 / std::sqrt(tgamma(static_cast<double>(n + 1)));
+  double normalisation_factor =
+      1.0 / std::sqrt(tgamma(static_cast<double>(n + 1)));
   for (unsigned i = 0; i < n; ++i) {
     exp = op * exp;
   }
@@ -65,21 +69,19 @@ namespace {
 
 template <class Info>
 struct ConjugateVisitor {
-  void operator() (ComplexNumber &num) const {
+  void operator()(ComplexNumber &num) const {
     num.value = std::conj(num.value);
   }
-  void operator() (Info &info) const {
-    info.complex_conjugate();
-  }
-  void operator() (NamedNumber &num) const {
+  void operator()(Info &info) const { info.complex_conjugate(); }
+  void operator()(NamedNumber &num) const {
     num.complex_conjugated = num.complex_conjugated xor true;
   }
-  void operator() (CompositeNumber &num) const {
+  void operator()(CompositeNumber &num) const {
     num = ConjugateExpr::create(std::move(num));
   }
 };
 
-}
+} // namespace
 
 template <class OperatorInfo>
 Operator<OperatorInfo> hermition_conjugate(Operator<OperatorInfo> op) {
@@ -88,42 +90,53 @@ Operator<OperatorInfo> hermition_conjugate(Operator<OperatorInfo> op) {
 }
 
 template <class OperatorInfo>
-Expression<OperatorInfo> hermition_conjugate(const Expression<OperatorInfo> & exp) {
+Expression<OperatorInfo>
+hermition_conjugate(const Expression<OperatorInfo> &exp) {
   Expression<OperatorInfo> new_exp;
   new_exp.expression.resize(exp.expression.size());
   for (unsigned i = 0; i < new_exp.expression.size(); ++i) {
     for (int j = exp.expression[i].size() - 1; j >= 0; --j) {
-      new_exp.expression[i].push_back(hermition_conjugate<OperatorInfo>(exp.expression[i][j]));
+      new_exp.expression[i].push_back(
+          hermition_conjugate<OperatorInfo>(exp.expression[i][j]));
     }
   }
   return new_exp;
 }
 
 template <class OperatorInfo>
-bool is_state_vector(const Operator<OperatorInfo> & A) {
-  if (A.is_number()) {return false;}
+bool is_state_vector(const Operator<OperatorInfo> &A) {
+  if (A.is_number()) {
+    return false;
+  }
   return A.info().type == Type::STATE_VECTOR;
 }
 template <class OperatorInfo>
-bool is_hc_state_vector(const Operator<OperatorInfo> & A) {
-  if (A.is_number()) {return false;}
+bool is_hc_state_vector(const Operator<OperatorInfo> &A) {
+  if (A.is_number()) {
+    return false;
+  }
   return A.info().type == Type::HC_STATE_VECTOR;
 }
 template <class OperatorInfo>
-bool is_creation_op(const Operator<OperatorInfo> & A) {
-  if (A.is_number()) {return false;}
+bool is_creation_op(const Operator<OperatorInfo> &A) {
+  if (A.is_number()) {
+    return false;
+  }
   return A.info().type == Type::CREATION_OPERATOR;
 }
 template <class OperatorInfo>
-bool is_anihilation_op(const Operator<OperatorInfo> & A) {
-  if (A.is_number()) {return false;}
+bool is_anihilation_op(const Operator<OperatorInfo> &A) {
+  if (A.is_number()) {
+    return false;
+  }
   return A.info().type == Type::ANIHILATION_OPERATOR;
 }
 template <class OperatorInfo>
-bool is_unspecified_op(const Operator<OperatorInfo> & A) {
-  if (A.is_number()) {return false;}
+bool is_unspecified_op(const Operator<OperatorInfo> &A) {
+  if (A.is_number()) {
+    return false;
+  }
   return A.info().type == Type::UNSPECIFIED;
 }
 
-} // end namspace
-
+} // namespace operators
