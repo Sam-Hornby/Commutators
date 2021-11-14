4214af89f8c7c9f610e413209f859646016090ee
Sam Hornby <samuelh@graphcore.ai>
2021-11-14 18:28:39 +0000

    add bogoliubov benchmark

--- tests/benchmarks/EvaluateBenchmark.cpp
+++ tests/benchmarks/EvaluateBenchmark.cpp
@@ -5,6 +5,8 @@
 #include <Expression/Ordering.hpp>
 #include <Comutators/BosonComutators.hpp>
 #include <Substitutions/Substitutions.hpp>
+#include <transforms/BogoliubovTransform.hpp>
+
 
 
 
@@ -41,9 +43,32 @@ BENCHMARK(evaluate_benchmark)->Apply(custom_arguments);
 
 
 static void bogoliubov_benchmark(benchmark::State& state) {
-  
+  Expression<Fock1DInfo> expression;
+  const auto E = named_number<Fock1DInfo>('E');
+  const auto Y = named_number<Fock1DInfo>('Y');
+  for (int group = -2; group < state.range(0); ++group) {
+    int index_0 = group;
+    int index_1 = group - 1000;
+    Expression<Fock1DInfo> grouped_expression = 
+        (E *
+            ((creation_op(index_0) *
+              anihilation_op(index_0))
+            + (creation_op(index_1) *
+               anihilation_op(index_1)))
+
+         + Y *
+            ((creation_op(index_0) *
+                creation_op(index_1))
+            + anihilation_op(index_1) *
+              anihilation_op(index_0)));
+    expression = expression + grouped_expression;
+  }
+  for (auto _ : state) {
+    auto result =
+      bogoliubov_transform(expression, OperatorType::BOSON);
+  }
 }
 
-BENCHMARK(bogoliubov_benchmark);
+BENCHMARK(bogoliubov_benchmark)->DenseRange(1, 5, 1);
 
 BENCHMARK_MAIN();
