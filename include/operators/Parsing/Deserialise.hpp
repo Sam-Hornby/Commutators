862489560466879bea748ee6fcab141545c467c7
Sam Hornby <samuelh@graphcore.ai>
2022-07-21 11:57:40 +0100

    add tokeniser

--- include/operators/Parsing/Deserialise.hpp
+++ include/operators/Parsing/Deserialise.hpp
@@ -2,8 +2,11 @@
 
 #include <Expression/Expression.hpp>
 #include <absl/algorithm/algorithm.h>
+#include <absl/algorithm/container.h>
 #include <string_view>
 #include <vector>
+#include <absl/strings/str_join.h>
+#include <cctype>
 
 namespace operators {
 // Function to take a string a return an expression from it
@@ -19,18 +22,74 @@ namespace operators {
 //     - [named_number]
 //     - {composite number}
 
-template <class OperatorInfo>
-struct OperandOrOp {
-  Expression<OperatorInfo> operand;
-  char op; 
+
+struct ParsingInfo {
+  const std::string special_chars{"+*-/!()[]{}"};
+
+  bool token_end(char c) const {
+    return absl::c_any_of(special_chars, [&] (char s) {
+      return s == c;
+    });
+  }
 };
+
+const ParsingInfo parsing_info;
 
 void check_input(const std::string & input) {
   // check brackets
 }
 
+bool end_of_current_token(std::string::const_iterator it) {
+  return std::isspace(*it) or parsing_info.token_end(*it);
+}
+
+template <class It>
+std::string_view create_view(It begin, It end) {
+  return std::string_view(&(*begin), std::distance(begin, end));
+}
+
+void remove_whitespace(std::vector<std::string_view> & result) {
+  auto last = std::remove_if(result.begin(), result.end(), [] (std::string_view tok) {
+    return tok.size() == 0 or std::isspace(tok[0]);
+  });
+  result.erase(last, result.end());
+}
+
+void find_pow_operators(std::vector<std::string_view> & result) {
+  for (std::size_t i = 1; i < result.size(); ++i) {
+    if (result[i-1] == "*" and result[i] == "*") {
+      result[i-1] = create_view(result[i-1].begin(),
+                                result[i].end());
+      result[i] = std::string_view();
+    }
+  }
+  remove_whitespace(result);
+}
+
 std::vector<std::string_view> tokenise_exp(const std::string & input) {
-  return {};
+  std::vector<std::string_view> result;
+
+  auto token_begin = input.begin();
+  for (auto it = input.begin(); it < input.end();) {
+    // commit last token to result
+    if (end_of_current_token(it)) {
+      result.emplace_back(create_view(token_begin, it));
+    } else {
+      ++it;
+      continue;
+    }
+    // put all the special chars into single tokens
+    while (it < input.end() and end_of_current_token(it)) {
+      // all specials are single characters so add them and increment
+      result.emplace_back(create_view(it, it + 1));
+      ++it;
+    }
+    token_begin = it;
+  }
+  result.emplace_back(create_view(token_begin, input.end()));
+  remove_whitespace(result);
+  find_pow_operators(result);
+  return result;
 }
 
 // For now just support *,+,(,) and forget about root, square etc
@@ -38,7 +97,7 @@ template <class OperatorInfo>
 Expression<OperatorInfo> from_string(std::string input) {
   check_input(input);
   auto tokens = tokenise_exp(input);
-
+  spdlog::debug("Tokens : {}", absl::StrJoin(tokens, ","));
   // Shunting yard algorithm to transform expression to postfix version
   // rules are:
   //   - operands are pushed to queue instantly
--- tests/ParseTests.cpp
+++ tests/ParseTests.cpp
@@ -7,6 +7,58 @@
 
 using namespace operators;
 
-BOOST_AUTO_TEST_CASE(parse_a) {
 
+
+Operator<Fock0DInfo> creation_op(char c = 'a') {
+  auto res = Operator<Fock0DInfo>(ordering_value(0),
+                  Fock0DInfo(Type::CREATION_OPERATOR));
+  res.info().symbol = c;
+  return res;
+}
+Operator<Fock0DInfo> anihilation_op(char c = 'a') {
+  auto res = Operator<Fock0DInfo>(ordering_value(0),
+                  Fock0DInfo(Type::ANIHILATION_OPERATOR));
+  res.info().symbol = c;
+  return res;
+}
+
+struct InputAndResult {
+  const char * input;
+  Expression<Fock0DInfo> result;
+};
+
+const std::vector<InputAndResult> test_cases = {
+  InputAndResult{"a_0", {{{anihilation_op()}}}},
+  InputAndResult{"a_0!", {{{creation_op()}}}},
+  InputAndResult{"c", {{{anihilation_op('c')}}}},
+  InputAndResult{"c!", {{{creation_op('c')}}}},
+  {"", {}},
+  {"a!**3", creation_op() * creation_op() * creation_op()},
+  {"a*b!+c_0*(d_0 + e!)",
+      anihilation_op('a') * creation_op('b') + anihilation_op('c')
+      * (anihilation_op('d') + creation_op('e'))},
+}; 
+
+BOOST_AUTO_TEST_CASE(parse_successful) {
+  for (const auto & test_case : test_cases) {
+    auto result = from_string<Fock0DInfo>(test_case.input);
+    BOOST_CHECK_EQUAL(result, test_case.result);
+  }
+}
+
+const std::vector<std::string> fail_cases = {
+  "abc_0", // to characters for op name
+  "a!a", // no sensical operator
+  "a b", // no operator between tokens
+  "(a", // unmatched bracket
+  "(a + [b)]", // invalid bracket sequence
+  "a + + b", // invalid sequence
+  "a *** 3", // invalid operator
+};
+
+BOOST_AUTO_TEST_CASE(parse_failed) {
+  for (const auto & test : fail_cases) {
+    BOOST_CHECK_THROW(from_string<Fock0DInfo>(test),
+                      std::logic_error);
+  }
 }
